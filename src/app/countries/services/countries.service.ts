dc4257547e8bdcff4aa1c7b5720d9dc3ed4ae025
Jonathan <contactoanonimo.x@gmail.com>
2024-09-30 13:54:50 -0600

    126. Try Catch

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { catchError, Observable, of } from 'rxjs';
 import { Country } from '../interfaces/country';
 
 @Injectable({ providedIn: 'root' })
@@ -12,5 +12,8 @@ export class CountriesService {
 
     const url = `${this.apiUrl}/capital/${term}`;
     return this.http.get<Country[]>(url)
+      .pipe(
+        catchError(error => of([]))
+      );
   }
 }
