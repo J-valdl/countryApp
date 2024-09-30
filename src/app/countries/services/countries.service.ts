3a9127072f7425282a5a72ccef55f210c002d86e
Jonathan <contactoanonimo.x@gmail.com>
2024-09-29 22:49:44 -0600

    country service

---
+++ src/app/countries/services/countries.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Country } from '../interfaces/country';
+
+@Injectable({ providedIn: 'root' })
+export class CountryService {
+  constructor(private http: HttpClient) { }
+  private apiUrl: string = 'https://restcountries.com/v3.1'
+
+  searchCapital(term: string): Observable<Country[]> {
+
+    const url = `${this.apiUrl}/capital/${term}`;
+    return this.http.get<Country[]>(url)
+  }
+}
