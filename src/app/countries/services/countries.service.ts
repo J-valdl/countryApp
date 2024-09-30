32944bad222d582e16bcb109b8413f6c80f83705
Jonathan <contactoanonimo.x@gmail.com>
2024-09-30 17:51:45 -0600

    Fin Seccion 9

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,12 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { catchError, Observable, of } from 'rxjs';
+import { catchError, map, Observable, of } from 'rxjs';
 import { Country } from '../interfaces/country';
 
 @Injectable({ providedIn: 'root' })
 export class CountriesService {
   constructor(private http: HttpClient) { }
   private apiUrl: string = 'https://restcountries.com/v3.1'
+
+  searchCountryAlphaCode(code: string): Observable<Country | null> {
+    const url = `${this.apiUrl}/alpha/${code}`;
+    return this.http.get<Country[]>(url)
+      .pipe(
+        map(countries => countries.length > 0 ? countries[0] : null)
+      );
+  }
 
   searchCapital(term: string): Observable<Country[]> {
     const url = `${this.apiUrl}/capital/${term}`;
